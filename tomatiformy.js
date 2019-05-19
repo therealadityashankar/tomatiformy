972617ad6399a4299c6382e3ef90366bc27ec1a2
Aditya Shankar <aditniru@gmail.com>
2019-05-19 20:08:09 +0530

    added support to convert json to forms, added need for npm package 'form-data'

--- tomatiformy.js
+++ tomatiformy.js
@@ -2,6 +2,8 @@
  * convert form data to json
  * and vice-versa
  */
+if (typeof window == 'undefined')
+    var FormData = require('form-data');
 
 /**
  * @namespace
@@ -17,7 +19,7 @@ var tomatoformy = {
      * @param {FormData} form - a form object
      * @param {object} lists - an array of strings containing all form items considered to be lists
      *
-     * @returns {object||undefined} returns undefined 
+     * @returns {object|undefined} returns undefined 
      */
     tomato: function(form, lists){
         if(form.checkValidity()){
@@ -35,6 +37,25 @@ var tomatoformy = {
         } else{
             return
         }
+    },
+
+    /**
+     * create form from a js object
+     * it was fun writing that
+     *
+     * @param {object} jsObj - the js object to maek into a form <br>
+     * thas rite folks make is now spelt maek <br>
+     * unless thats an abusive word of some kind <br>
+     * then sorry <br>
+     * I'm too lazy to  google right now <br>
+     */
+    formy: function(jsObj){
+        var formData = new FormData();
+        for(var key in jsObj){
+            formData.append(key, jsObj[key]);
+        }
+
+        return formData;
     }
 }
 
